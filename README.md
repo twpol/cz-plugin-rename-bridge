2c8810b8b3c693df3687fc33f4a39cac2566dcc0
James Ross <git@james-ross.co.uk>
2018-11-08 18:17:03 +0000

    feat: Add initial rename-bridge code, with list of bridge users, docs and example

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # ChatZilla Plugin: Rename Bridge
 
 This plugin for [ChatZilla](http://chatzilla.hacksrus.com/) transforms IRC-bridge users into their own unique users.
+
+## Renaming pattern
+
+To rename the user of a message, the message text must match the regular expression `^<(.*?)> ([^ ].*)$`. Group 1 is the user name and group 2 is their message. If matched, the message will appear entirely to be from the user name matched by this expression and not the bridge user.
+
+## Preferences
+
+* `bridges` _string, space-separated list of IRC URLs_ (default `""`)
+
+  Specifies the list of bridge users, e.g. `"ircs://irc.example.com/db,isnick ircs://irc.example.com/slack_bot,isnick"` specifies two users (`db` and `slack_bot`) on the same server (`ircs://irc.example.com/`). The bridge users do not need to all be on one server.
---
+++ init.js
@@ -0,0 +1,67 @@
+/* global client, plugin */
+/* global formatException */
+
+// PLUGIN ENVIRONMENT //
+
+plugin.id = 'rename-bridge';
+
+plugin.init =
+function _init(glob) {
+    this.major = 1;
+    this.minor = 0;
+    this.version = this.major + '.' + this.minor + ' (08 Nov 2018)';
+    this.description = 'Transforms IRC-bridge users into their own unique users. ' +
+    "By James Ross <chatzilla-plugins@james-ross.co.uk>.";
+
+    this.prefary.push(['bridges', '', '']);
+
+    return 'OK';
+}
+
+plugin.enable =
+function _enable() {
+    client.eventPump.addHook([
+        { set: 'server', type: 'notice' },
+        { set: 'server', type: 'privmsg' }
+    ],
+        plugin.onMessage,
+        plugin.id + '-server-privmsg+notice');
+    plugin.onPrefChanged();
+    return true;
+}
+
+plugin.disable =
+function _disable() {
+    client.eventPump.removeHookByName(plugin.id + '-server-privmsg+notice');
+    return true;
+}
+
+plugin.onPrefChanged =
+function _onprefchanged() {
+    plugin.bridges = plugin.prefs['bridges'].split(' ')
+        .filter(bridge => !!bridge)
+        .map(parseIRCURL)
+        .filter(url => url.isnick);
+}
+
+plugin.onMessage =
+function _onmessage(e) {
+    try {
+        // client.display('RB source=' + e.server.parent.getURL() + ' user=' + e.user.unicodeName + ' params.length=' + e.params.length + ' params[1]=' + e.params[1] + ' params[2]=' + e.params[2] + '');
+        const bridges = plugin.bridges;
+        for (const bridge of bridges) {
+            if ((bridge.host === e.server.parent.unicodeName || (bridge.host === e.server.hostname && bridge.port === e.server.port)) && e.user.unicodeName === bridge.target) {
+                const bridgeMessage = e.params[2].match(/^<(.*?)> ([^ ].*)$/);
+                if (bridgeMessage) {
+                    e.bridgeUser = e.user;
+                    e.user = new CIRCUser(e.server, null, bridgeMessage[1]);
+                    e.params[2] = bridgeMessage[2];
+                    client.display('RB ' + e.bridgeUser.unicodeName + ' --> ' + e.user.unicodeName);
+                }
+                break;
+            }
+        }
+    } catch (ex) {
+        client.display('Rename Bridge: ' + formatException(ex));
+    }
+}
